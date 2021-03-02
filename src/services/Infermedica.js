bc6e0078433f4ce0c03036254efc7beb221c085e
GrantClark209 <grantclark1999@gmail.com>
2021-03-01 20:22:49 -0800

    Logging for Infermedica

--- src/services/Infermedica.js
+++ src/services/Infermedica.js
@@ -10,6 +10,14 @@ class Infermedica {
   }
 
   async parse(text) {
+    const reqBody = JSON.stringify({
+      age: { value: this.age },
+      sex: this.sex,
+      text: text,
+      concept_types: ['condition', 'risk_factor', 'symptom'],
+      context: this.context,
+    });
+    console.log('[ Infermedica ] Request: ', reqBody);
     const response = await fetch('https://api.infermedica.com/v3/parse', {
       method: 'POST',
       headers: {
@@ -17,18 +25,14 @@ class Infermedica {
         'App-Key': INFERMEDICA_APP_KEY,
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({
-        age: { value: this.age },
-        sex: this.sex,
-        text: text,
-        concept_types: ['condition', 'risk_factor', 'symptom'],
-        context: this.context,
-      }),
+      body: reqBody,
     });
-    const { mentions } = await response.json();
-    console.log('[ Infermedica ]', JSON.stringify(mentions));
 
-    const symptoms = mentions.filter(({ type }) => type === 'symptom');
+    const response = await response.json();
+    const { mentions } = response;
+    console.log('[ Infermedica ] Response: ', JSON.stringify(response));
+
+    const symptoms = mentions?.filter(({ type }) => type === 'symptom') ?? [];
     // Use the last mentioned symptom as the context for best accuracy
     if (symptoms.length > 0) {
       this.context = [symptoms[symptoms.length - 1]];
